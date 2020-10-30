52982df1717381a787548d019421019bec3d907e
skondoker <kondoker@sledge.cs.ucr.edu>
2020-10-30 15:22:49 -0700

    Lab1

---
+++ xv6/ln.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 3){
+    printf(2, "Usage: ln old new\n");
+    // exit();
+    exit(0);
+  }
+  if(link(argv[1], argv[2]) < 0)
+    printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+  // exit();
+  exit(1);
+}
